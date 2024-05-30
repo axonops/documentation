0b431546ae7a35d1766694a1f9ffe547266406a0
brian.stark <brian.stark@digitalis.io>
2024-05-30 09:20:43 +0100

    Deployed 04a9291 with MkDocs version: 1.5.3

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -5,3 +5,56 @@ function random(){
 
   
 window.onload = random;
+
+const tabSync = () => {
+  document.getElementsByName('osFamily')[0].checked = true;
+  document.getElementsByName('casFamily')[0].checked = true;
+  const tabs = document.querySelectorAll(".tabbed-set > input")
+  for (const tab of tabs) {
+    tab.addEventListener("change", function() {
+      const tabInner = document.querySelector(`label[for=${tab.id}]`).innerHTML
+      // Check if Debian or RedHat
+      if(tab.checked && tabInner.includes("Debian"))
+        document.getElementsByName('osFamily')[0].checked = true;
+      if(tab.checked && tabInner.includes("RedHat"))
+        document.getElementsByName('osFamily')[1].checked = true;
+      
+      // Check Cassandra and Debian
+      if(tab.checked && tabInner.includes("Cassandra 3.0") && tabs[0].checked){
+        document.getElementsByName('osFamily')[0].checked = true;
+        document.getElementsByName('casFamily')[0].checked = true;
+      }
+      if(tab.checked && tabInner.includes("Cassandra 3.11") && tabs[0].checked){
+        document.getElementsByName('osFamily')[0].checked = true;
+        document.getElementsByName('casFamily')[1].checked = true;
+      }
+      if(tab.checked && tabInner.includes("Cassandra 4.0") && tabs[0].checked){
+        document.getElementsByName('osFamily')[0].checked = true;
+        document.getElementsByName('casFamily')[2].checked = true;
+      }
+      if(tab.checked && tabInner.includes("Cassandra 4.1") && tabs[0].checked){
+        document.getElementsByName('osFamily')[0].checked = true;
+        document.getElementsByName('casFamily')[3].checked = true;
+      }
+
+      // Check Cassandra and RedHat
+      if(tab.checked && tabInner.includes("Cassandra 3.0") && tabs[1].checked){
+        document.getElementsByName('osFamily')[1].checked = true;
+        document.getElementsByName('casFamily')[0].checked = true;
+      }
+      if(tab.checked && tabInner.includes("Cassandra 3.11") && tabs[1].checked){
+        document.getElementsByName('osFamily')[1].checked = true;
+        document.getElementsByName('casFamily')[1].checked = true;
+      }
+      if(tab.checked && tabInner.includes("Cassandra 4.0") && tabs[1].checked){
+        document.getElementsByName('osFamily')[1].checked = true;
+        document.getElementsByName('casFamily')[2].checked = true;
+      }
+      if(tab.checked && tabInner.includes("Cassandra 4.1") && tabs[1].checked){
+        document.getElementsByName('osFamily')[1].checked = true;
+        document.getElementsByName('casFamily')[3].checked = true;
+      }
+    });
+  }
+}
+window.onload = tabSync;
